e40cce4165f80ab326836be07956dff3adced5e4
Danil4UA <146213395+Danil4UA@users.noreply.github.com>
2025-02-05 19:48:29 +0200

    add splide

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,61 +1,33 @@
-import React, { useState, useRef } from "react";
-import "./Gallery.css";
+import React from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 import Image from "next/image";
+import "./Gallery.css";
 
 interface GalleryProps {
   images: string[];
 }
 
 const Gallery = ({ images }: GalleryProps) => {
-  const [mainImage, setMainImage] = useState(images[0]);
-  const thumbnailsRef = useRef<HTMLDivElement>(null);
-
-  const handleSwipe = (e: React.TouchEvent<HTMLDivElement>) => {
-    if (!thumbnailsRef.current) return;
-
-    const container = thumbnailsRef.current;
-    const startX = e.touches[0].clientX;
-    let lastX = startX;
-
-    const handleTouchMove = (moveEvent: TouchEvent) => {
-      const moveX = moveEvent.touches[0].clientX;
-      const deltaX = lastX - moveX;
-      lastX = moveX;
-
-      container.scrollLeft += deltaX;
-    };
-
-    const handleTouchEnd = () => {
-      // Clean up event listeners
-      document.removeEventListener("touchmove", handleTouchMove);
-      document.removeEventListener("touchend", handleTouchEnd);
-    };
-
-    // Attach touch move and end listeners
-    document.addEventListener("touchmove", handleTouchMove);
-    document.addEventListener("touchend", handleTouchEnd);
-  };
-
   return (
-    <div className="Gallery__wrapper">
-      {/* Main image */}
-      <div className="Gallery__main-image">
-        <Image src={mainImage} width={500} height={500} alt="Main" className="Gallery__main-image-content" />
-      </div>
-
-      <div className="Gallery__thumbnails" ref={thumbnailsRef} onTouchStart={handleSwipe}>
-        {images.map((image, index) => (
-          <Image
-            key={index}
-            width={80}
-            height={80}
-            src={image}
-            alt={`Thumbnail ${index + 1}`}
-            className={`Gallery__thumbnail ${mainImage === image ? "Gallery__thumbnail--active" : ""}`}
-            onClick={() => setMainImage(image)}
-          />
+    <div>
+      <Splide
+        options={{
+          type: "loop",
+          perPage: 1,
+          perMove: 1,
+          autoplay: false,
+          interval: 3000,
+          arrows: true,
+          pagination: true
+        }}
+      >
+        {images.map((src, index) => (
+          <SplideSlide key={index}>
+            <Image src={src} width={500} height={500} alt={`Image ${index + 1}`} className="responsive-image" />
+          </SplideSlide>
         ))}
-      </div>
+      </Splide>
     </div>
   );
 };
