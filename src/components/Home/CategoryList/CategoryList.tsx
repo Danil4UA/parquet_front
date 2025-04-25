b5fdb03d5e553b557898651c3c2d2f5a0f9c1001
Danil4UA <146213395+Danil4UA@users.noreply.github.com>
2025-04-25 12:50:07 +0300

    fixing photos

--- src/components/Home/CategoryList/CategoryList.tsx
+++ src/components/Home/CategoryList/CategoryList.tsx
@@ -1,5 +1,13 @@
 import CategoryCard from "../CategoryCard/CategoryCard";
 import { useTranslations } from "next-intl";
+import categoryFlooring from "/public/assets/category_flooring_new.jpg"
+import categoryLaminate from "/public/assets/category_laminate.jpg"
+import categorySpc from "/public/assets/category_spc.jpg"
+import categoryWood from "/public/assets/category_wood.jpg"
+import categoryCatalog from "/public/assets/category_catalog.jpg"
+import categoryPanel from "/public/assets/category_panel.jpg"
+import categorySeeling from "/public/assets/category_seeling.jpg"
+
 import "./CategoryList.css";
 
 export type Category = {
@@ -13,43 +21,43 @@ const CategoryList = () => {
   const t = useTranslations("Categories");
   const categories: Category[] = [
     {
-      image: "/assets/category_flooring_new.jpg",
+      image: categoryFlooring.src,
       path: "/products/all",
       title: t("catalog_title"),
       description: t("catalog_description")
     },
     {
-      image: "/assets/category_laminate.jpg",
+      image: categoryLaminate.src,
       path: "/products/laminate",
       title: t("laminate_title"),
       description: t("laminate_description")
     },
     {
-      image: "/assets/category_spc.jpg",
+      image: categorySpc.src,
       path: "/products/spc",
       title: t("spc_title"),
       description: t("spc_description")
     },
     {
-      image: "/assets/category_wood.jpg",
+      image: categoryWood.src,
       path: "/products/wood",
       title: t("wood_title"),
       description: t("wood_description")
     },
     {
-      image: "/assets/category_catalog.jpg",
+      image: categoryCatalog.src,
       path: "/products/sales",
       title: t("sales_title"),
       description: t("sales_description")
     },
     {
-      image: "/assets/category_panel.jpg",
+      image: categoryPanel.src,
       path: "/products/panels",
       title: t("panels_title"),
       description: t("panels_description")
     },
     {
-      image: "/assets/category_seeling.jpg",
+      image: categorySeeling.src,
       path: "/products/cladding",
       title: t("cladding_title"),
       description: t("cladding_description")
