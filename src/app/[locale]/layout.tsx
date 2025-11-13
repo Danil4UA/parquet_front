0b40c988cf23d86460f503cb7fdb09caf875b0b8
Danil4UA <146213395+Danil4UA@users.noreply.github.com>
2025-11-13 15:59:37 +0200

    adding google tag manager

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -30,6 +30,17 @@ export default async function RootLayout({
   return (
     <html lang={locale} dir={locale === "he" ? "rtl" : "ltr"}>
       <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PSFBJ888');
+          `}
+        </Script>
         <Script id="utm-capture" strategy="beforeInteractive">
           {`
             (function() {
@@ -143,6 +154,14 @@ export default async function RootLayout({
         </noscript>
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PSFBJ888"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ReactQueryProvider>
           <ReduxProvider>
               <NextIntlClientProvider messages={messages}>
